7e3e1ead103f7ed84be5b2b81f25c8abdc8becd6
JuyeongJayPark <royalbless@naver.com>
2022-07-14 17:11:05 +0900

    first commit

---
+++ Test/rawdata_download.sh
@@ -0,0 +1,12 @@
+mkdir TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread01/220603_A00930_0309_AHLG2CDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread01/220608_A00479_0354_BHLG7LDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread01/220620_A00930_0315_AHLFYLDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread01/220627_A00479_0363_BHLG33DSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread01/220704_A00930_0324_BHKY5KDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread01/220711_A00479_0369_AHL7JLDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread02/220613_A00479_0355_BHLGJYDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread02/220623_A00479_0361_AHLFT5DSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread02/220707_A00930_0325_AHLFG2DSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread03/220616_A00930_0314_AHL5WNDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
+rsync -PLrvh --progress --partial jypark@qc1:/rawread03/220630_A00930_0323_AHLFCVDSX3/Unaligned/TBO220569/TN2205D2373* ./TN2205D2373
---
+++ rawdata_download.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd 
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--user', default="jypark")
+args = parser.parse_args()
+
+
+user = args.user
+
+cwd = os.getcwd()
+Files = os.listdir(cwd)
+NIPTON_Files = [File for File in Files if File.startswith("NIPTON")]
+NIPTON_File = NIPTON_Files[0]
+
+NIPTON_df = pd.read_csv(NIPTON_File, header=0, skiprows=[1])
+
+NIPTON_df_essential = NIPTON_df[['대상정보','데이터']]
+
+TN_list = NIPTON_df_essential['대상정보'].tolist()
+
+output_bash = open('rawdata_download.sh', 'w')
+
+for TN_ID in TN_list:
+    output_bash.write(f"mkdir {TN_ID}\n")
+    run_info_object = NIPTON_df_essential[NIPTON_df_essential['대상정보'] == TN_ID]['데이터'].str.split("\n").tolist()
+    run_info_list = sum(run_info_object,[])
+    for run_info in run_info_list:
+        run_info_path = run_info.split(":")[1]
+        output_bash.write(f"rsync -PLrvh --progress --partial {user}@qc1-120g:{run_info_path}/{TN_ID}*.gz ./{TN_ID}\n")
+#        output_bash.write(f"scp {user}@qc1-120g:{run_info_path}/{TN_ID}*.gz ./{TN_ID}\n")
